5c0b44fc381ba287a7f90d1838e8b6afa99fa9c8
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-05-03 22:50:36 +0700

    Latihan

---
+++ Manusia.java
@@ -0,0 +1,27 @@
+package inheritance;
+
+
+
+public class Manusia {
+  String mata = "Melihat";
+  String sakit = "Tidak Ikut Ujian";
+    public void makan() {
+        System.out.println("Manusia Makan");
+    }
+    
+    public void minum() {
+        System.out.println("Manusia Minum");
+    }
+    
+    public void lari() {
+        System.out.println("Manusia ari");
+    }
+    
+    public void melihat() {
+        System.out.println("Manusia "+mata);
+    }
+    
+    public void sakit(){
+        System.out.println("Manusia "+sakit);
+    }
+}
