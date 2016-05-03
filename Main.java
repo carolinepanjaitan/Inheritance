319fe3f8c3a26ee0553276b0036be7edeb9abec7
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-05-03 22:54:51 +0700

    Latihan

---
+++ Main.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Ali ali = new Ali();
+        ali.melihat();
+        ali.matematika();
+        ali.sakit();
+    }
+}
